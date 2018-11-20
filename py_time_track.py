c9d1c96998b0e4c8bff42e55ce1e88a27884b728
Nick Olivier <Olivier_N@lynchburg.edu>
2018-11-20 13:33:57 -0600

    Add files via upload

---
+++ py_time_track.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def seconds_to_time(sec):
+    if (sec // 3600) == 0:
+        HH = '00'
+    elif (sec // 3600) < 10:
+        HH = '0' + str(int(sec // 3600))
+    else:
+        HH = str(int(sec // 3600))
+    min_raw = (np.float64(sec) - (np.float64(sec // 3600) * 3600)) // 60
+    if min_raw < 10:
+        MM = '0' + str(int(min_raw))
+    else:
+        MM = str(int(min_raw))
+    sec_raw = (sec - (np.float64(sec // 60) * 60))
+    if sec_raw < 10:
+        SS = '0' + str(int(sec_raw))
+    else:
+        SS = str(int(sec_raw))
+    return HH + ':' + MM + ':' + SS + ' (hh:mm:ss)'
+
+def sec_to_time_elapsed(end_tm, start_tm, return_time = False):
+    sec_elapsed = (np.float64(end_tm) - np.float64(start_tm))
+    if return_time:
+        return seconds_to_time(sec_elapsed)
+    else:
+        print('Execution Time: ' + seconds_to_time(sec_elapsed))
